ff7564c6dea406814ce26ccbd6903422131a348b
Iniubong Peter <peteriniubong@gmail.com>
2023-11-16 09:35:05 +0100

    update

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -39,249 +39,249 @@ defineProps({
 				</template>
 			</div>
 		</div>
-		<!-- ====== Hero Section Start -->
-		<div
-			class="relative bg-white dark:bg-dark pt-[120px] pb-[110px] lg:pt-[150px]"
-		>
-			<div class="container mx-auto">
-				<div class="flex flex-wrap items-center -mx-4">
-					<div class="w-full px-4 lg:w-5/12">
-						<div class="flex items-center mb-4">
-							<div class="w-36">
-								<img src="/images/ejp.png" alt="hero" class="w-full" />
+		<div class="mt-14 w-full max-w-full">
+			<!-- ====== Hero Section Start -->
+			<div class="relative bg-white dark:bg-dark pt-20 pb-[110px] w-full">
+				<div class="container mx-auto">
+					<div class="flex flex-wrap items-center">
+						<div class="w-full px-6 lg:w-6/12">
+							<div class="flex items-center mb-4">
+								<div class="w-36">
+									<img src="/images/ejp.png" alt="hero" class="w-full" />
+								</div>
 							</div>
-						</div>
-						<div class="hero-content">
-							<h1
-								class="mb-5 text-4xl font-bold !leading-[1.208] text-dark dark:text-white sm:text-[42px] lg:text-[40px] xl:text-5xl"
-							>
-								Welcome to our <br />
-								Employee Incentive Program.
-							</h1>
-							<p
-								class="mb-8 max-w-[480px] text-base text-gray-500 dark:text-dark-6"
-							>
-								Efforts make the difference, and this is our way of saying thank
-								you to each other. Explore your rewards, gift others and let's
-								celebrate our success!
-							</p>
-							<div v-if="canLogin" class="my-14">
-								<Link
-									v-if="$page.props.auth.user"
-									:href="route('dashboard')"
-									class="inline-flex items-center justify-center px-6 py-3 text-base font-medium text-center text-white rounded-md bg-[#982069] hover:bg-blue-dark lg:px-7"
-									>Dashboard</Link
+							<div class="hero-content">
+								<h1
+									class="mb-5 text-4xl font-bold !leading-[1.208] text-dark dark:text-white sm:text-[42px] lg:text-[40px] xl:text-5xl"
 								>
-
-								<template v-else>
+									Welcome to our <br />
+									Employee Incentive Program.
+								</h1>
+								<p
+									class="mb-8 max-w-[480px] text-base text-gray-500 dark:text-dark-6"
+								>
+									Efforts make the difference, and this is our way of saying
+									thank you to each other. Explore your rewards, gift others and
+									let's celebrate our success!
+								</p>
+								<div v-if="canLogin" class="my-14">
 									<Link
-										:href="route('login')"
-										class="inline-flex items-center justify-center px-6 py-3 text-base font-medium text-center text-[#982069] rounded-md hover:bg-blue-dark lg:px-7 border-2 border-[#982069]"
-										>Log in</Link
+										v-if="$page.props.auth.user"
+										:href="route('dashboard')"
+										class="inline-flex items-center justify-center px-6 py-3 text-base font-medium text-center text-white rounded-md bg-[#982069] hover:bg-blue-dark lg:px-7"
+										>Dashboard</Link
 									>
 
-									<Link
-										v-if="canRegister"
-										:href="route('register')"
-										class="ml-4 inline-flex items-center justify-center px-6 py-3 text-base font-medium text-center text-white rounded-md bg-[#982069] hover:bg-blue-dark lg:px-7"
-										>Register</Link
-									>
-								</template>
+									<template v-else>
+										<Link
+											:href="route('login')"
+											class="inline-flex items-center justify-center px-6 py-3 text-base font-medium text-center text-[#982069] rounded-md hover:bg-blue-dark lg:px-7 border-2 border-[#982069]"
+											>Log in</Link
+										>
+
+										<Link
+											v-if="canRegister"
+											:href="route('register')"
+											class="ml-4 inline-flex items-center justify-center px-6 py-3 text-base font-medium text-center text-white rounded-md bg-[#982069] hover:bg-blue-dark lg:px-7"
+											>Register</Link
+										>
+									</template>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="hidden px-4 lg:block lg:w-1/12"></div>
-					<div class="w-full px-4 lg:w-6/12">
-						<div class="lg:ml-auto lg:text-right">
-							<div class="relative z-10 inline-block pt-11 lg:pt-0">
-								<img
-									src="https://cdn.tailgrids.com/2.0/image/marketing/images/hero/hero-image-01.png"
-									alt="hero"
-									class="max-w-full lg:ml-auto"
-								/>
-								<span class="absolute -left-8 -bottom-8 z-[-1]">
-									<svg
-										width="93"
-										height="93"
-										viewBox="0 0 93 93"
-										fill="none"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3" />
-										<circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3" />
-										<circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3" />
-										<circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3" />
-										<circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3" />
-										<circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3" />
-										<circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3" />
-										<circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3" />
-										<circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3" />
-										<circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3" />
-										<circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3" />
-										<circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3" />
-										<circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3" />
-										<circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3" />
-										<circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3" />
-										<circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3" />
-										<circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3" />
-										<circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3" />
-										<circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3" />
-										<circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3" />
-										<circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3" />
-										<circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3" />
-										<circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3" />
-										<circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3" />
-										<circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
-									</svg>
-								</span>
+						<div class="w-full px-6 lg:w-6/12">
+							<div class="lg:ml-auto lg:text-right">
+								<div class="relative z-10 inline-block pt-11 lg:pt-0">
+									<img
+										src="https://cdn.tailgrids.com/2.0/image/marketing/images/hero/hero-image-01.png"
+										alt="hero"
+										class="max-w-full lg:ml-auto"
+									/>
+									<span class="absolute -left-8 -bottom-8 z-[-1]">
+										<svg
+											width="93"
+											height="93"
+											viewBox="0 0 93 93"
+											fill="none"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<circle cx="2.5" cy="2.5" r="2.5" fill="#3056D3" />
+											<circle cx="2.5" cy="24.5" r="2.5" fill="#3056D3" />
+											<circle cx="2.5" cy="46.5" r="2.5" fill="#3056D3" />
+											<circle cx="2.5" cy="68.5" r="2.5" fill="#3056D3" />
+											<circle cx="2.5" cy="90.5" r="2.5" fill="#3056D3" />
+											<circle cx="24.5" cy="2.5" r="2.5" fill="#3056D3" />
+											<circle cx="24.5" cy="24.5" r="2.5" fill="#3056D3" />
+											<circle cx="24.5" cy="46.5" r="2.5" fill="#3056D3" />
+											<circle cx="24.5" cy="68.5" r="2.5" fill="#3056D3" />
+											<circle cx="24.5" cy="90.5" r="2.5" fill="#3056D3" />
+											<circle cx="46.5" cy="2.5" r="2.5" fill="#3056D3" />
+											<circle cx="46.5" cy="24.5" r="2.5" fill="#3056D3" />
+											<circle cx="46.5" cy="46.5" r="2.5" fill="#3056D3" />
+											<circle cx="46.5" cy="68.5" r="2.5" fill="#3056D3" />
+											<circle cx="46.5" cy="90.5" r="2.5" fill="#3056D3" />
+											<circle cx="68.5" cy="2.5" r="2.5" fill="#3056D3" />
+											<circle cx="68.5" cy="24.5" r="2.5" fill="#3056D3" />
+											<circle cx="68.5" cy="46.5" r="2.5" fill="#3056D3" />
+											<circle cx="68.5" cy="68.5" r="2.5" fill="#3056D3" />
+											<circle cx="68.5" cy="90.5" r="2.5" fill="#3056D3" />
+											<circle cx="90.5" cy="2.5" r="2.5" fill="#3056D3" />
+											<circle cx="90.5" cy="24.5" r="2.5" fill="#3056D3" />
+											<circle cx="90.5" cy="46.5" r="2.5" fill="#3056D3" />
+											<circle cx="90.5" cy="68.5" r="2.5" fill="#3056D3" />
+											<circle cx="90.5" cy="90.5" r="2.5" fill="#3056D3" />
+										</svg>
+									</span>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-		<!-- ====== Hero Section End -->
+			<!-- ====== Hero Section End -->
 
-		<div
-			class="relative sm:flex sm:justify-center sm:items-center bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-[#982069] selection:text-white"
-		>
-			<div class="max-w-7xl mx-auto p-6 lg:p-8 my-28">
-				<div class="mt-16">
-					<div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-						<a
-							href="https://www.ejusticeproject.com/"
-							target="_blank"
-							class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-[#982069]"
-						>
-							<div>
-								<div
-									class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full"
-								>
-									<svg
-										fill="none"
-										class="w-7 h-7 stroke-[#982069]"
-										stroke-width="1.5"
-										viewBox="0 0 24 24"
-										xmlns="http://www.w3.org/2000/svg"
-										aria-hidden="true"
+			<div
+				class="relative sm:flex sm:justify-center sm:items-center bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-[#982069] selection:text-white"
+			>
+				<div class="max-w-7xl mx-auto p-6 lg:p-8 my-28">
+					<div class="mt-16">
+						<div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
+							<a
+								href="https://www.ejusticeproject.com/"
+								target="_blank"
+								class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-[#982069]"
+							>
+								<div>
+									<div
+										class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full"
 									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
-										></path>
-									</svg>
+										<svg
+											fill="none"
+											class="w-7 h-7 stroke-[#982069]"
+											stroke-width="1.5"
+											viewBox="0 0 24 24"
+											xmlns="http://www.w3.org/2000/svg"
+											aria-hidden="true"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
+											></path>
+										</svg>
+									</div>
+
+									<h2
+										class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"
+									>
+										E-Justice Project
+									</h2>
+
+									<p
+										class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
+									>
+										E-Justice Project. A California law firm focused on
+										empowering historically underserved communities by educating
+										and advocating for their employment-related rights. They
+										provide legal assistance, including negotiations and
+										litigation, to ensure equitable access to justice for
+										California workers.
+									</p>
 								</div>
 
-								<h2
-									class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="2"
+									class="self-center shrink-0 stroke-[#982069] w-6 h-6 mx-6"
 								>
-									E-Justice Project
-								</h2>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+									/>
+								</svg>
+							</a>
 
-								<p
-									class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
-								>
-									E-Justice Project. A California law firm focused on empowering
-									historically underserved communities by educating and
-									advocating for their employment-related rights. They provide
-									legal assistance, including negotiations and litigation, to
-									ensure equitable access to justice for California workers.
-								</p>
-							</div>
-
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="2"
-								class="self-center shrink-0 stroke-[#982069] w-6 h-6 mx-6"
+							<a
+								href="https://ejpapc.com"
+								target="_blank"
+								class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-[#982069]"
 							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-								/>
-							</svg>
-						</a>
-
-						<a
-							href="https://ejpapc.com"
-							target="_blank"
-							class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-[#982069]"
-						>
-							<div>
-								<div
-									class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full"
-								>
-									<svg
-										class="w-7 h-7"
-										version="1.1"
-										id="Capa_1"
-										xmlns="http://www.w3.org/2000/svg"
-										xmlns:xlink="http://www.w3.org/1999/xlink"
-										viewBox="0 0 24 24"
-										xml:space="preserve"
-										fill="#982069"
+								<div>
+									<div
+										class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full"
 									>
-										<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-										<g
-											id="SVGRepo_tracerCarrier"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-										></g>
-										<g id="SVGRepo_iconCarrier">
-											<g>
-												<path
-													style="fill: #982069"
-													d="M2,6.577v4.461h16.189l1.396-1.395c0.44-0.439,1.158-0.439,1.597,0l1.396,1.395h1.499v2h-1.5 l-1.394,1.394c-0.44,0.44-1.158,0.44-1.597,0l-1.395-1.394H2V17.5h1.42l1.397-1.396c0.44-0.439,1.158-0.439,1.597,0L7.811,17.5 h16.266v2H7.808l-1.395,1.394c-0.439,0.44-1.157,0.44-1.596,0L3.422,19.5H2v4.538H0.077H0v-24h0.077H2v4.539h6.958l1.396-1.396 c0.44-0.439,1.158-0.439,1.597,0l1.397,1.396h10.729v2h-10.73l-1.395,1.394c-0.439,0.44-1.157,0.44-1.596,0L8.96,6.577H2z"
-												></path>
+										<svg
+											class="w-7 h-7"
+											version="1.1"
+											id="Capa_1"
+											xmlns="http://www.w3.org/2000/svg"
+											xmlns:xlink="http://www.w3.org/1999/xlink"
+											viewBox="0 0 24 24"
+											xml:space="preserve"
+											fill="#982069"
+										>
+											<g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+											<g
+												id="SVGRepo_tracerCarrier"
+												stroke-linecap="round"
+												stroke-linejoin="round"
+											></g>
+											<g id="SVGRepo_iconCarrier">
+												<g>
+													<path
+														style="fill: #982069"
+														d="M2,6.577v4.461h16.189l1.396-1.395c0.44-0.439,1.158-0.439,1.597,0l1.396,1.395h1.499v2h-1.5 l-1.394,1.394c-0.44,0.44-1.158,0.44-1.597,0l-1.395-1.394H2V17.5h1.42l1.397-1.396c0.44-0.439,1.158-0.439,1.597,0L7.811,17.5 h16.266v2H7.808l-1.395,1.394c-0.439,0.44-1.157,0.44-1.596,0L3.422,19.5H2v4.538H0.077H0v-24h0.077H2v4.539h6.958l1.396-1.396 c0.44-0.439,1.158-0.439,1.597,0l1.397,1.396h10.729v2h-10.73l-1.395,1.394c-0.439,0.44-1.157,0.44-1.596,0L8.96,6.577H2z"
+													></path>
+												</g>
 											</g>
-										</g>
-									</svg>
+										</svg>
+									</div>
+
+									<h2
+										class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"
+									>
+										EJPAPC
+									</h2>
+
+									<p
+										class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
+									>
+										A comprehensive timeline tool that methodically chronicles
+										the sequence of events in your legal case. Effectively
+										manage the evolution of your case, ensuring a thorough grasp
+										of the proceedings and maintaining control over every
+										significant development.
+									</p>
 								</div>
 
-								<h2
-									class="mt-6 text-xl font-semibold text-gray-900 dark:text-white"
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									class="self-center shrink-0 stroke-[#982069] w-6 h-6 mx-6"
 								>
-									EJPAPC
-								</h2>
-
-								<p
-									class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed"
-								>
-									A comprehensive timeline tool that methodically chronicles the
-									sequence of events in your legal case. Effectively manage the
-									evolution of your case, ensuring a thorough grasp of the
-									proceedings and maintaining control over every significant
-									development.
-								</p>
-							</div>
-
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke-width="1.5"
-								class="self-center shrink-0 stroke-[#982069] w-6 h-6 mx-6"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
-								/>
-							</svg>
-						</a>
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"
+									/>
+								</svg>
+							</a>
+						</div>
 					</div>
-				</div>
 
-				<div class="mt-16 px-6">
-					<div
-						class="float-right ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0"
-					>
-						E-Justice Project Employee Incentive Program v{{ appVersion }}
+					<div class="mt-16 px-6">
+						<div
+							class="float-right ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0"
+						>
+							E-Justice Project Employee Incentive Program v{{ appVersion }}
+						</div>
+						<div class="clear-both"></div>
 					</div>
-					<div class="clear-both"></div>
 				</div>
 			</div>
 		</div>
